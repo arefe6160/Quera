5ab4e78b83de4564dcc092ca3bfe8a6d6bc493d2
arefe6160 <arefedadashi6160@gmail.com>
2025-12-09 10:51:43 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   int a,b;
+   cin>>a>>b;
+   int m[a][b];
+
+   for(int i=0;i<a;i++){
+    for(int j=0;j<b;j++){
+        cin>>m[i][j];
+    }
+   }
+   int energy=0;
+
+    for(int i=0;i<a;++i){
+        for(int j=0;j<b;++j){
+            if(j<b-1) {
+                int k=m[i][j]-m[i][j+1];
+                energy+=(k<0) ? -k : k;
+            }
+            if(i<a-1) {
+                int k= m[i][j]-m[i+1][j];
+                energy+=(k<0) ? -k: k;
+            }
+        }
+    }
+    cout<<energy<<endl;
+
+}
