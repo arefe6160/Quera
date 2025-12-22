8725b1ab35c5f4ddb8fe80e0f70e5f1d25426206
Arefe Dadashi <arefedadashi6160@gmail.com>
2025-12-22 13:46:10 +0330

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+bool aval(int a){
+    if (a<2) return false;
+
+    for(int i=2;i<a;i++){
+        if (a%i==0){
+            return false;
+        }
+}return true;
+
+}
+int main()
+{int a,b;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++){
+        if (aval(i)==true){
+            cout<<i<<" ";
+        }
+    }cout<<endl;
+}
